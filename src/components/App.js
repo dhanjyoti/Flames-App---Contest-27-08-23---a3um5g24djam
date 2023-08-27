4d68e64f0b580b4aa8383a214749e17fe0701b6d
dhanjyoti <dhanjyotir20@gmail.com>
2023-08-27 11:11:08 +0530

    WIP

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,74 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+const removeChar = (str = '', index) => {
+    return str.substring(0, index) + str.substring(index + 1)
 }
+const App = () => {
+
+    const [firstName, setFirstName] = useState("");
+    const [secondName, setSecondName] = useState("");
+    const [result, setResult] = useState("");
+
+    const calculateRelationship = () => {
+
+        if(firstName.trim() === "" || secondName.trim() === ""){
+            setResult("Please enter valid input")
+            return
+        }
+        let fName = firstName
+        let sName = secondName
+        Array.from(firstName).forEach((val, index) => {
+            if (sName.includes(val)) {
+                sName = removeChar(sName, sName.indexOf(val))
+                fName = removeChar(fName, fName.indexOf(val))
+            }
+        })
+
+        const r = Math.ceil((fName.length + sName.length) / 6)
+
+        
+        switch (r) {
+            case 0:
+                setResult('Siblings')
+                break
+            case 1:
+                setResult("Friends");
+                break;
+            case 2:
+                setResult("Love")
+                break;
+            case 3:
+                setResult("Affection");
+                break
+            case 4:
+                setResult("Marriage")
+                break
+            case 5:
+                setResult("Enemy")
+                break
+            default:
+                setResult('Please enter valid input')
+        }
+    }
+
+    return (
+        <div id="main">
+            <input data-testid="input1" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+            <input data-testid="input2" value={secondName} onChange={(e) => setSecondName(e.target.value)} />
+            <button data-testid="calculate_relationship" onClick={calculateRelationship}>Calculate Relationship</button>
+            <button data-testid="clear" onClick={() => {
+                setFirstName('')
+                setSecondName('')
+                setResult('')
+            }}>Clear</button>
+            <h3 data-testid="answer">{result}</h3>
+            {/* Do not remove the main div */}
+        </div>
+    )
+}
+
 
 
 export default App;
